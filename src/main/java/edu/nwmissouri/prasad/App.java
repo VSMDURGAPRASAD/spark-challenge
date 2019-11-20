50caf5c54ec35bb2150a7247d2239a4c7c88ad04
SAI MANIKANTA DURGA PRASAD VINUKONDA <sai1996.vinukonda@gmail.com>
2019-11-19 18:00:41 -0600

    Added Source code

---
+++ src/main/java/edu/nwmissouri/prasad/App.java
@@ -0,0 +1,53 @@
+package edu.nwmissouri.prasad;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+import java.util.Arrays;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.Comparator;
+
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+    private static void methodfile(String inputfile){
+        SparkConf sparkConf = new SparkConf() .setMaster("local").setAppName("Challenge");
+             JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+            
+        //JavaSparkContext sparkContext =new JavaSparkContext();
+        JavaRDD<String> inputFile = sparkContext.textFile(inputfile);
+        JavaRDD<String> wordsFromFile = inputFile.flatMap( line -> Arrays.asList(line.split(" ")).iterator());
+        JavaPairRDD<String, Integer> countData = wordsFromFile.mapToPair(word -> new Tuple2(word, 1)).reduceByKey((x, y) -> (int) x + (int) y);
+        JavaPairRDD<Integer, String> output =countData.mapToPair(p -> new Tuple2(p._2,p._1)).sortByKey(Comparator.reverseOrder());
+
+        String outputFolder ="data";
+        Path path = FileSystems.getDefault().getPath(outputFolder);
+        FileUtils.deleteQuietly(path.toFile());
+        output.saveAsTextFile(outputFolder);
+        sparkContext.close();
+
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        if (args.length!=1){
+            System.out.println("Provide Arguments"+text(Challenge));
+
+
+        }
+        System.out.println()
+    }
+}
